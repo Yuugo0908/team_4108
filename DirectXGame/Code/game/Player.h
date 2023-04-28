bcfff457c14fb0a74b17774896524bce82b6fb6d
sekishotaro <k020g1492@g.neec.ac.jp>
2023-04-28 12:52:02 +0900

    自機作成/横移動/ジャンプ/重力実装

--- DirectXGame/Code/game/Player.h
+++ DirectXGame/Code/game/Player.h
@@ -12,43 +12,33 @@
 class Player
 {
 public: // メンバ関数
-
+	//初期化処理
 	bool Initialize(const XMFLOAT3 pos, const XMFLOAT3 scale);
-
-	void Update(bool rFlag, bool moveFlag);
-
-	void Rush(XMFLOAT3 targetPos, bool& flag, float& avoidTime);
-
-	void Avoid();
-
-	void Jump();
-
-	bool Damage(const std::unique_ptr<Object3d>& object);
-
-	void Reset();
-
-	void ReSpawn();
-
-	void TrackRot(const XMFLOAT3& startPos, const XMFLOAT3& endPos);
-
-	// マップ当たり判定
-	bool MapCollide(XMFLOAT3 boxPos, XMFLOAT3 boxScale);
-
-	bool StageCollide(XMFLOAT3 stagePos, XMFLOAT3 stageScale, bool& reverseFlag);
-
-	bool PoleCollide(XMFLOAT3 polePos, XMFLOAT3 poleScale);
-
+	// 更新処理
+	void Update();
 	// オブジェクト
 	const std::unique_ptr<Object3d>& GetObj() { return playerObj; }
-	// 回避
-	const bool& GetAvoidFlag() { return avoidFlag; }
-	void SetAvoidFlag(bool avoidFlag) { this->avoidFlag = avoidFlag; }
-	// 着地
-	const bool& GetOnGround() { return onGround; }
-	void SetOnGround(bool onGround) { this->onGround = onGround; }
-
-	// ジャンプ
-	void SetJumpFlag(bool jumpFlag) { this->jumpFlag = jumpFlag; }
+	
+	//Geter
+	bool& GetOnGround() { return onGround; }
+	float& GetmoveY() { return moveY; }
+private:
+	/// <summary>
+	/// 移動処理
+	/// </summary>
+	void MoveProcess();
+	/// <summary>
+	/// ジャンプ処理
+	/// </summary>
+	void JumpProcess();
+	/// <summary>
+	/// 重力処理
+	/// </summary>
+	void GravityProcess();
+	/// <summary>
+	/// 地面衝突判定処理
+	/// </summary>
+	void GroundCollisionProcess();
 
 private: // メンバ変数
 
@@ -66,26 +56,9 @@ private: // メンバ変数
 	XMFLOAT3 pPosOld = {};
 	XMFLOAT3 pScale = {};//大きさ
 	XMFLOAT3 pRot = {};//回転
-	XMFLOAT3 inertiaSave = {}; // 慣性
 	XMFLOAT3 reSpawnPos = {};
-
-	float pSpeed = 0.35f;
-	bool onGround = false;//自由落下のフラグ
-	bool jumpFlag = false;
-	float pMove = 0.0f;//移動量
-	float pAcc = 0.2f;//加速
-	float pVel = 0.2f;//速度
-	float pDown = 0.0f;
-	float pGra = 0.1f;//重力
-	float rate = 1.0f; // 斜め移動時の制限
-	int damageInterval = 0; // 攻撃を受けた際のインターバル
-
-	// 突進用
-	bool avoidFlag = false; // 回避開始フラグ
-	int avoidCount = 0;
-
-	// カメラ距離取得用
-	XMFLOAT3 cameraTrack = {};
-	float cameraRot = 0.0f;
+	XMFLOAT3 move = {}; //移動量
+	float moveY = 0.0f; //ジャンプ及び重力
+	bool onGround = false;
 };
 
